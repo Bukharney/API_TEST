bab1ea4e228795be1b61baea3a804ea4b770b865
Jirapat <bukharney@gmail.com>
2023-05-14 16:36:29 +0700

    first commit

---
+++ app/database.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# while True:
+#     try:
+#         conn = psycopg2.connect(
+#             host="localhost",
+#             database="fastapi",
+#             user="postgres",
+#             password="junepoon",
+#             port="5432",
+#             cursor_factory=RealDictCursor.RealDictCursor,
+#         )
+#         cur = conn.cursor()
+#         print("Database connected")
+#         break
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print("Error while connecting to PostgreSQL:", error)
+#         time.sleep(5)
