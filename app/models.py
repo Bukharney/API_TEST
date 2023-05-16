2425fbdfb78c6ecfca1400b3bbb1bbc78e295383
Jirapat <bukharney@gmail.com>
2023-05-17 00:09:47 +0700

    news null

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from sqlalchemy import (
     Column,
     Integer,
     String,
-    Boolean,
     ForeignKey,
     Float,
     BigInteger,
@@ -13,23 +12,6 @@ from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 from .database import Base
-
-
-class Post(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
-    published = Column(Boolean, server_default="TRUE", nullable=False)
-    created_at = Column(
-        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
-    )
-    owner_id = Column(
-        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
-    )
-
-    owner = relationship("User")
 
 
 class User(Base):
@@ -169,7 +151,7 @@ class Dividend(Base):
 class News(Base):
     __tablename__ = "news"
     id = Column(Integer, primary_key=True, nullable=False)
-    file = Column(String, nullable=False)
+    file = Column(String)
     topic = Column(String, nullable=False)
     content = Column(String, nullable=False)
     news_time = Column(TIMESTAMP(timezone=True), nullable=False)
