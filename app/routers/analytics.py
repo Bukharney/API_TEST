638fa6ea6f8c368a1ba5f89ced2aca7a7dfa0128
Jirapat <bukharney@gmail.com>
2023-05-29 14:33:58 +0700

    add 1st analye

--- app/routers/analytics.py
+++ app/routers/analytics.py
@@ -1,6 +1,7 @@
+import datetime
 from typing import List
 from fastapi import Depends, status, HTTPException, APIRouter
-from sqlalchemy import func
+from sqlalchemy import case, func, text
 from app import oauth2, utils
 from .. import models, schemas
 from ..database import get_db
@@ -14,17 +15,38 @@ def get_all_accounts(
     current_user: int = Depends(oauth2.get_current_user),
     db: Session = Depends(get_db),
 ):
-    accounts = db.query(
-        models.Orders.symbol,
-        models.Orders.volume,
-        models.Orders.price,
-        func.count(func.case([(models.Orders.side == "Buy", 1)], else_=None)).label(
-            "buy_count"
-        ),
-        func.count(func.case([(models.Orders.side == "Sell", 1)], else_=None)).label(
-            "sell_count"
-        ),
-    ).group_by(models.Orders.symbol, models.Orders.volume, models.Orders.price)
+    start_time = datetime.datetime(2023, 5, 29, 13, 00)
+    end_time = datetime.datetime(2023, 5, 29, 14, 5)
 
-    result = accounts.all()
-    return accounts
+    sql = text(
+        """
+    SELECT
+        symbol,
+        SUM(volume) AS vol_sum,
+        price,
+        COUNT(CASE WHEN side = 'Buy' THEN 1 END) AS total_buy_order,
+        COUNT(CASE WHEN side = 'Sell' THEN 1 END) AS total_sell_order
+    FROM
+        orders
+    WHERE 
+        time BETWEEN :start_time AND :end_time
+    GROUP BY
+        symbol,
+        volume,
+        price;
+    """
+    ).bindparams(start_time=start_time, end_time=end_time)
+
+    result = db.execute(sql).fetchall()
+
+    orders = [
+        {
+            "symbol": row.symbol,
+            "vol_sum": row.vol_sum,
+            "price": row.price,
+            "total_buy_order": row.total_buy_order,
+            "total_sell_order": row.total_buy_order,
+        }
+        for row in result
+    ]
+    return orders
